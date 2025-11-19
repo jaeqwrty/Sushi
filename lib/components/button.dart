e2b7dd6ccfae380d729fe3fbc34af780b79b75dc
jaeqwrty <jaesmalla1@gmail.com>
2025-11-19 09:46:03 +0800

    Inital commit

---
+++ lib/components/button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final String text;
+  const MyButton({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color.fromARGB(255, 153, 62, 55),
+        borderRadius: BorderRadius.circular(35),
+      ),
+      padding: EdgeInsets.all(20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(text, style: TextStyle(color: Colors.white)),
+
+          const SizedBox(height: 10),
+
+          Icon(Icons.arrow_forward, color: Colors.white),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/components/button.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 116, 42, 37),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const SizedBox(height: 10),
+            //ShopName
+            Text(
+              'SushiMan',
+              style: GoogleFonts.dmSerifDisplay(
+                fontSize: 28,
+                color: Colors.white,
+              ),
+            ),
+
+            const SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: Image.asset('assets/image/bento.png'),
+            ),
+
+            const SizedBox(height: 10),
+            Text(
+              'THE TASTE OF JAPANESE FOOD',
+              style: GoogleFonts.dmSerifDisplay(
+                color: Colors.white,
+                fontSize: 44,
+              ),
+            ),
+            Text(
+              'Feel the most popular Japanese food from anywhere and anytime',
+              style: TextStyle(
+                color: const Color.fromARGB(255, 194, 189, 189),
+                height: 2,
+              ),
+            ),
+            const SizedBox(height: 10),
+            MyButton(text: 'Get Started'),
+          ],
+        ),
+      ),
+    );
+  }
+}
